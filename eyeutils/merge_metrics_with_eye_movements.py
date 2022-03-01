81f480d2fe45e62ce9da0b6c3b8fffb83fcc6327
Omer S <omer.shubi@gmail.com>
2022-03-01 18:33:39 +0200

    fix - get words from eye tracking report instead of huggingface
    This way it is assured that there are the same number of words.

--- eyeutils/merge_metrics_with_eye_movements.py
+++ eyeutils/merge_metrics_with_eye_movements.py
@@ -18,31 +18,28 @@ def add_metrics_to_eye_tracking(eye_tracking_data: pd.DataFrame, tokenizer, mode
     >>> tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     >>> model = GPT2LMHeadModel.from_pretrained('gpt2')
     >>> et_data_enriched = add_metrics_to_eye_tracking(eye_tracking_data=et_data, tokenizer=tokenizer, model=model)
-    """
 
-    # Load the OneStopQA HuggingFace dataset
-    dataset = load_dataset(path='onestop_qa', split='train')
-    df = dataset.to_pandas()
-    df['level'] = dataset.features['level'].int2str(dataset['level'])
+    """
 
     # Extract metrics for all paragraph-level pairs
     metric_dfs = []
-    for row in df[['paragraph', 'paragraph_index', 'title', 'level']].drop_duplicates().itertuples():
-
-        print(row.Index, row.paragraph)
-        merged_df = get_metrics(text=row.paragraph, tokenizer=tokenizer, model=model)
-        merged_df['paragraph_id'] = row.paragraph_index + 1
-        merged_df['article_title'] = row.title.replace("’", "'")
+    without_duplicates = eye_tracking_data[['paragraph_id', 'article_title', 'level', 'IA_ID', 'IA_LABEL']].drop_duplicates()
+    text_from_et = without_duplicates.groupby(['paragraph_id', 'article_title', 'level'])['IA_LABEL'].apply(list)
+    text_from_et = text_from_et.apply(lambda text: " ".join(text))
+    for row in text_from_et.reset_index().itertuples():
+        print(row.Index, row.IA_LABEL)
+        merged_df = get_metrics(text=row.IA_LABEL, tokenizer=tokenizer, model=model)
+        merged_df['paragraph_id'] = row.paragraph_id
+        merged_df['article_title'] = row.article_title
         merged_df['level'] = row.level
         merged_df.reset_index(inplace=True)
         merged_df = merged_df.rename({'index': 'IA_ID', 'Word': 'IA_LABEL'}, axis=1)
         metric_dfs.append(merged_df)
-        # TODO handle duplicate words with '-' in report.
     metric_df = pd.concat(metric_dfs, axis=0)
 
     # Join metrics with eye_tracking_data
     et_data_enriched = eye_tracking_data.merge(metric_df, how='left',
-                                               on=['article_title', 'paragraph_id', 'level', 'IA_ID'], # TODO add IA_LABEL after fixing '-'
+                                               on=['article_title', 'paragraph_id', 'level', 'IA_ID'],
                                                validate='many_to_one')
 
     return et_data_enriched
@@ -54,4 +51,3 @@ if __name__ == '__main__':
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     model = GPT2LMHeadModel.from_pretrained('gpt2')
     et_data_enriched = add_metrics_to_eye_tracking(eye_tracking_data=et_data, tokenizer=tokenizer, model=model)
-    pass
