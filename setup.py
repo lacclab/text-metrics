37d9faca51b23c2a945ac7c6cd91874e534191fc
Omer Shubi <omer.shubi@gmail.com>
2024-01-21 19:24:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 # TODO improve with https://python-poetry.org/
 setup(
     name='text_metrics',
-    version='0.9.1',
+    version='0.9.2',
     packages=['text_metrics'],
     url='https://github.com/lacclab/text-metrics',
     license='',
