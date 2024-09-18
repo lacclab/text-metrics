99d51289074932cb1dcd86f66d88b616be1df50d
meiri.yoav (nlp20) <meiri.yoav@campus.technion.ac.il>
2024-09-18 11:01:59 +0300

    Fix surprisal extractor

--- text_metrics/surprisal_extractors.py
+++ text_metrics/surprisal_extractors.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List, Tuple
 import numpy as np
-from text_metrics.utils import init_tok_n_model
+from utils import init_tok_n_model, remove_redundant_left_context
 import torch
 
 
@@ -38,7 +38,8 @@ class SurprisalExtractor:
     def surprise(
         target_text: str,
         left_context_text: str | None = None,
-        stride: int = 512,
+        overlap_size: int = 512,
+        allow_overlap: bool = False,
     ) -> Tuple[np.ndarray, List[Tuple[int]]]:
         NotImplementedError
 
@@ -120,23 +121,49 @@ class CatCtxLeftSurpExtractor(SurprisalExtractor):
         self,
         target_text: str,
         left_context_text: str | None = None,
-        stride: int = 512,
+        overlap_size: int | None = None,
+        allow_overlap: bool = False,
     ) -> Tuple[np.ndarray, List[Tuple[int]]]:
         """This function calculates the surprisal of a target_text using a language model.
-        In case the target_text is too long, it is split into chunks while keeping previous context of STRIDE tokens.
+         In case the target_text is too long, it is split into chunks while keeping previous context of STRIDE tokens.
+         Args:
+             target_text (str): The text to calculate surprisal for.
+             left_context_text (str | None, optional): A left context to be concatenated with the target_text BEFORE PASSED THROUGH THE MODEL. Defaults to None.
+             overlap_size (int | None, optional): In case the target_text + left_context is too long, it is split into chunks with overlap_size tokens. Defaults to None.
+             allow_overlap (bool, optional): If True, the target_text will be split into chunks with overlap_size tokens. Defaults to False.
 
-        Args:
-            target_text (str): The target_text for which the surprisal is calculated
-            model (Union[AutoModelForCausalLM, GPTNeoXForCausalLM]): The language model from which the surprisal is calculated
-            tokenizer (Union[AutoTokenizer, GPTNeoXTokenizerFast]): The tokenizer for the language model
-            model_name (str): The name of the language model (e.g "gpt2", "gpt-neo-125M", "opt-125m", "pythia-1b")
-            left_context_text (str, optional): The left context text. Defaults to None.
-            stride (int, optional): The number of tokens to keep as context. Defaults to 512.
+         Raises:
+             ValueError: In case the target_text is too long and allow_overlap is False.
 
-        Returns:
-            Tuple[np.ndarray, List[Tuple[int]]]: The surprisal values for each token in the target_text, the offset mapping
-            The offset mapping is a list of tuples, where each tuple contains the start and end character index of the token
+         Returns:
+             Tuple[np.ndarray, List[Tuple[int]]]: The surprisal values of the target_text and the offset mapping of the target_text.
+
+         Example:
+         target_text = "Angela Erdmann never knew her grandfather. He died in 1946, six years before she was born. But, on Tuesday 8th April,
+         2014, she described the extraordinary moment when she received a message in a bottle, 101 years after he had lobbed it into the Baltic Sea.
+         Thought to be the world's oldest message in a bottle, it was presented to Erdmann by the museum that is now exhibiting it in Germany."
+
+         left_context_text = 'How did Angela Erdmann find out about the bottle?'
+
+         Returns:
+         target_text_log_probs = array([4.97636890e+00, 5.20506799e-01, 6.58840360e-03, 5.63895130e+00,
+            2.56178689e+00, 3.68980312e+00, 4.96654510e+00, 3.15608168e+00,
+            3.41664624e+00, 3.26654100e+00, 9.50351954e-01, 5.09015131e+00,
+            ...
+            3.03512335e+00, 9.98807430e+00, 1.23654938e+00, 1.98460269e+00,
+            4.46555328e+00, 3.09026152e-01], dtype=float32)
+
+        target_text_offset_mapping = [(0, 7), (7, 11), (11, 15), (15, 21), (21, 26), (26, 30), (30, 42), (42, 43),
+        (43, 46), (46, 51), (51, 54), (54, 59), (59, 60), (60, 64), (64, 70), (70, 77), (77, 81), (81, 85),
+        ....]
+
+        ! Notice that the offset mapping is non-inclusice on the right index (it says 0-7 but actially the first word is chars 0-6)
         """
+        # assert that if allow_overlap is True, overlap_size is not None
+        if allow_overlap:
+            assert overlap_size is not None, "overlap_size must be specified"
+        full_context = left_context_text + " " + target_text
+
         with torch.no_grad():
             all_log_probs = torch.tensor([], device=self.model.device)
             offset_mapping = []
@@ -146,26 +173,34 @@ class CatCtxLeftSurpExtractor(SurprisalExtractor):
             except AttributeError:
                 max_ctx = int(1e6)
 
+            # only surprisal for the target text will be extracted, so if the left_context is longer
+            # than the max context, we remove the redundant left context tokens that cannot be used in-context with the target text
+            if left_context_text is not None:
+                left_context_text = remove_redundant_left_context(
+                    self.tokenizer,
+                    left_context_text=left_context_text,
+                    max_ctx_in_tokens=max_ctx,
+                )
+
             assert (
-                stride < max_ctx
-            ), f"Stride size {stride} is larger than the maximum context size {max_ctx}"
+                overlap_size < max_ctx
+            ), f"Stride size {overlap_size} is larger than the maximum context size {max_ctx}"
 
             # print(max_ctx)
             accumulated_tokenized_text = []
             while True:
                 encodings = self.tokenizer(
-                    target_text[start_ind:],
+                    full_context[start_ind:],
                     max_length=max_ctx - 2,
                     truncation=True,
                     return_offsets_mapping=True,
                     add_special_tokens=False,
                 )
                 is_last_chunk = (encodings["offset_mapping"][-1][1] + start_ind) == len(
-                    target_text
+                    full_context
                 )
 
                 tensor_input = self._create_input_tokens(
-                    self.tokenizer,
                     encodings,
                     start_ind,
                     is_last_chunk,
@@ -173,11 +208,11 @@ class CatCtxLeftSurpExtractor(SurprisalExtractor):
                 )
 
                 log_probs, shift_labels = self._tokens_to_log_probs(
-                    self.model, tensor_input, is_last_chunk
+                    tensor_input, is_last_chunk
                 )
 
                 # Handle the case where the target_text is too long for the model
-                offset = 0 if start_ind == 0 else stride - 1
+                offset = 0 if start_ind == 0 else overlap_size - 1
                 all_log_probs = torch.cat([all_log_probs, log_probs[offset:]])
                 accumulated_tokenized_text += shift_labels[offset:]
 
@@ -195,26 +230,46 @@ class CatCtxLeftSurpExtractor(SurprisalExtractor):
                 if start_ind == 0:
                     "If we got here, the context is too long"
                     # find the context length in tokens
-                    context_length = len(self.tokenizer.encode(target_text))
-                    print(
-                        f"The context length is too long ({context_length}>{max_ctx}) for {self.model_name}. Splitting the target_text into chunks with stride {stride}"
-                    )
+                    context_length = len(self.tokenizer.encode(full_context))
+                    if allow_overlap:
+                        print(
+                            f"The context length is too long ({context_length}>{max_ctx}) for {self.model_name}. Splitting the full text into chunks with overlap {overlap_size}"
+                        )
+                    else:
+                        raise ValueError(
+                            f"The context length is too long ({context_length}>{max_ctx}) for {self.model_name}. Try enabling allow_overlap and specify overlap size"
+                        )
 
-                start_ind += encodings["offset_mapping"][-stride - 1][1]
+                start_ind += encodings["offset_mapping"][-overlap_size - 1][1]
 
         # The accumulated_tokenized_text is the text we extract surprisal values for
         # It is after removing the BOS/EOS tokens
         # Make sure the accumulated_tokenized_text is equal to the original target_text
         assert (
             accumulated_tokenized_text
-            == self.tokenizer(target_text, add_special_tokens=False)["input_ids"]
+            == self.tokenizer(full_context, add_special_tokens=False)["input_ids"]
         )
 
         all_log_probs = np.asarray(all_log_probs.cpu())
 
-        assert all_log_probs.shape[0] == len(offset_mapping)
+        # cut all_log_probs and offset_mapping to the length of the target_text ONLY (without the left context)
+        # notice that accumulated_tokenized_text contains both the left context and the target_text
+        # so we need to remove the left context from it
+        target_text_len = len(self.tokenizer(target_text)["input_ids"])
+        target_text_log_probs = all_log_probs[
+            -target_text_len + 1 :
+        ]  # because we concatenate the left context to the target text
 
-        return all_log_probs, offset_mapping
+        target_text_offset_mapping = offset_mapping[-target_text_len + 1 :]
+        offset_mapping_onset = target_text_offset_mapping[0][0]
+        target_text_offset_mapping = [
+            (i - offset_mapping_onset, j - offset_mapping_onset)
+            for i, j in target_text_offset_mapping
+        ]
+
+        assert target_text_log_probs.shape[0] == len(target_text_offset_mapping)
+
+        return target_text_log_probs, target_text_offset_mapping
 
 
 class SoftCatCtxAggSurpExtractor(SurprisalExtractor):
