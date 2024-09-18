9dab59ab158098b0c4fc0bdf91a09031fd9f678c
meiri.yoav (nlp20) <meiri.yoav@campus.technion.ac.il>
2024-09-18 11:02:55 +0300

    Setup + tests paths

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
+
 # TODO improve with https://python-poetry.org/
 setup(
-    name='text_metrics',
-    version='1.1.2',
-    packages=['text_metrics'],
-    url='https://github.com/lacclab/text-metrics',
-    license='',
-    author='Omer Shubi',
-    author_email='omer.shubi@gmail.com',
-    description='Utils for Eye Tracking Measurements',
+    name="text_metrics",
+    version="1.1.2",
+    packages=["text_metrics"],
+    url="https://github.com/lacclab/text-metrics",
+    license="",
+    author="Omer Shubi",
+    author_email="omer.shubi@gmail.com",
+    description="Utils for Eye Tracking Measurements",
     include_package_data=True,
-    package_data={'': ['data/*.tsv']},
-    extras_require = {
-            'lm_zoo': ['lm-zoo']
-        },
+    package_data={"": ["data/*.tsv"]},
+    extras_require={"lm_zoo": ["lm-zoo"]},
     install_requires=[
-        'pandas>2.1.0',
-        'transformers>=4.40.1',
-        'wordfreq>=3.0.3',
-        'numpy>=1.20.3',
-        'torch',
-        'spacy',
-        'accelerate'
-    ]
+        "pandas>2.1.0",
+        "transformers>=4.40.1",
+        "wordfreq>=3.0.3",
+        "numpy>=1.20.3",
+        "torch",
+        "spacy",
+        "accelerate",
+    ],
 )
