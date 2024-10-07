7f37986256f11166f53d563199d12238f43ef2d9
meiri.yoav (laccl-srv1) <meiri.yoav@campus.technion.ac.il>
2024-10-07 18:18:48 +0300

    package imports issues

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ from setuptools import setup
 setup(
     name="text_metrics",
     version="1.1.2",
-    packages=["text_metrics", "text_metrics.surprisal_extractors"],
+    packages=[
+        "text_metrics",
+        "text_metrics.surprisal_extractors",
+        "text_metrics.pimentel_word_prob",
+    ],
     url="https://github.com/lacclab/text-metrics",
     license="",
     author="Omer Shubi",
