d983bb3897211dc9dc07de070efee607c6bdc172
meiri.yoav (laccl-srv1) <meiri.yoav@campus.technion.ac.il>
2024-10-08 10:14:53 +0300

    Refactor package imports in setup.py and remove unused import

--- setup.py
+++ setup.py
@@ -1,46 +1,19 @@
-from setuptools import setup, find_packages, find_namespace_packages
-from setuptools.command.install import install
-import subprocess
-import os
-
-
-class CustomInstallCommand(install):
-    """Custom command to initialize submodules before installation."""
-
-    def run(self):
-        # Ensure we are in the root directory of the project
-        project_root = os.path.abspath(os.path.dirname(__file__))
-        # Run git submodule update --init --recursive
-        subprocess.check_call(
-            ["git", "submodule", "update", "--init", "--recursive", "--verbose"],
-            cwd=project_root,
-        )
-
-        # add "echo hihihihi"
-        subprocess.check_call(
-            ["echo", "hihihihi"],
-        )
-        # Proceed with the standard installation
-        install.run(self)
+from setuptools import setup
 
 
 # TODO improve with https://python-poetry.org/
 setup(
     name="text_metrics",
     version="1.1.2",
-    packages=find_namespace_packages(),
-    # [
-    #     "text_metrics",
-    #     "text_metrics.surprisal_extractors",
-    #     # pimentel_word_prob is a submodule of text_metrics
-    #     # "text_metrics.pimentel_word_prob",
-    #     # "text_metrics.pimentel_word_prob.wordsprobability",
-    #     # "text_metrics.pimentel_word_prob.wordsprobability.models",
-    #     # "text_metrics.pimentel_word_prob.wordsprobability.utils",
-    # ],
-    cmdclass={
-        "install": CustomInstallCommand,
-    },
+    packages=[
+        "text_metrics",
+        "text_metrics.surprisal_extractors",
+        # pimentel_word_prob is a submodule of text_metrics
+        "text_metrics.pimentel_word_prob",
+        "text_metrics.pimentel_word_prob.wordsprobability",
+        "text_metrics.pimentel_word_prob.wordsprobability.models",
+        "text_metrics.pimentel_word_prob.wordsprobability.utils",
+    ],
     url="https://github.com/lacclab/text-metrics",
     license="",
     author="Omer Shubi",
